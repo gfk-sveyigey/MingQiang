d22ae2a248f9ab9e3c23ac6c715395615fe80c85
gfk-sveyigey <121384281+gfk-sveyigey@users.noreply.github.com>
2025-02-14 12:47:57 +0800

    v0.1.3
    .

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # 版本
-version = "0.1.2"
+version = "0.1.3"
 
 # 是否开启debug模式
 DEBUG = False
--- mingqiang/services/house.py
+++ mingqiang/services/house.py
@@ -252,7 +252,7 @@ def detail(house: Union[int, House], user: Union[int, User]):
         else:
             raw["hearted"] = 1 if house.id in [collention.id for collention in user.collections] else 2
 
-        if house.group_id in [group.id for group in user.groups]:
+        if user.supervisor or house.group_id in [group.id for group in user.groups]:
             owner = services.user.get(house.owner_id)
             group = services.group.get(house.group_id)
             raw["owner"] = f"{group.name} - {owner.nickname}"
